d2b09e45ca993b1c6de94db544ecbd7b1b4c84e9
Sudeep Prasad <sudeep.rp@gmail.com>
2023-08-15 19:54:52 +0530

    vitals

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,43 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+from time import sleep
+import sys
 
+
+def vitals_ok(temperature, pulseRate, spo2):
+  if temperature > 102 or temperature < 95:
+    print('Temperature critical!')
+    for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
+    return False
+  elif pulseRate < 60 or pulseRate > 100:
+    print('Pulse Rate is out of range!')
+    for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
+    return False
+  elif spo2 < 90:
+    print('Oxygen Saturation out of range!')
+    for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
+    return False
   return True
 
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(vitals_ok(99, 102, 70) is False)
+  assert(vitals_ok(98.1, 70, 98) is True)
+  print('Done')
